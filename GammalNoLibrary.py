9e58606e16b4ba05177b65cf3a86ecb3b031b206
DianaXimena <dianaxime0@gmail.com>
2020-10-04 23:26:58 -0600

    Done

--- GammalNoLibrary.py
+++ GammalNoLibrary.py
@@ -9,19 +9,11 @@ Cifrado de información
 #Maria Ines Vasquez 18250
 """
 import random  
-from math import pow
-import generate_prime as Prime
+from math import pow, gcd
+from Crypto.Util.number import getPrime
+import os
   
 a = random.randint(2, 10) 
-
-def gcd(a, b): 
-    if a < b: 
-        return gcd(b, a) 
-    elif a % b == 0: 
-        return b; 
-    else: 
-        return gcd(b, a % b) 
-
 
 # Se generan numeros grandes random
 def gen_key(q): 
@@ -39,7 +31,7 @@ def powerMod(a, b, c):
     y = a 
   
     while b > 0: 
-        if b % 2 == 0: 
+        if b % 2 == 0:
             x = (x * y) % c; 
         y = (y * y) % c 
         b = int(b / 2) 
@@ -48,18 +40,24 @@ def powerMod(a, b, c):
   
 # Encriptacion simetrica del mensaje con q, h y g 
 def encrypt(msg, q, h, g): 
-  
+    # q es p
+    # h es K
+    # g es g
     en_msg = [] 
-  
-    k = gen_key(q)# la llave privada 
+    
+    # k es b
+    k = gen_key(q)# la llave privada
+    print("La llave privada (k) de Bob es: ", k) 
+    # s es y2
     s = powerMod(h, k, q) #Se realiza la exponenciacion modular con h
+    # p es y1
     p = powerMod(g, k, q) #Se realiza la exponenciacion modular con g
       
     for i in range(0, len(msg)): 
         en_msg.append(msg[i]) #se guarda msg caracter por caracter
   
     print("g**k usado : ", p) 
-    print("g**ak usadp : ", s) 
+    print("g**ak usado : ", s) 
     for i in range(0, len(en_msg)): 
         en_msg[i] = s * ord(en_msg[i]) 
   
@@ -80,58 +78,27 @@ def main():
   
     msg = 'esto es facil de encriptar'
     print("Mensaje original :", msg) 
-  
-    q = random.randint(pow(10, 20), pow(10, 50)) # se genera q con numeros random grandes
+    # q es p
+    q = getPrime(100) # se genera q con numeros random grandes
+    print("p numero primo usado : ", q)
     g = random.randint(2, q) # se genera g en base a q
-  
+    # key es a 
     key = gen_key(q)# se crea la llave privada
+    # K 
     h = powerMod(g, key, q) #Se realiza la exponenciacion modular 
-    print("g usado : ", g) 
+    
+    print("g usado : ", g)
+    print("La llave privada (a) de Alice es: ", a) 
     print("g**a usado : ", h) 
   
     en_msg, p = encrypt(msg, q, h, g) # se encripta el mensaje con q, h, g
+    print("Mensaje Cifrado :", en_msg)
     dr_msg = decrypt(en_msg, p, key, q) # se desencripta el mensaje con p, la llave y q
     dmsg = ''.join(dr_msg)  # se concatena el mensaje y se imprime
-    print("Mensaje Descifrado :", dmsg); 
+    print("Mensaje Descifrado :", dmsg)
   
   
 if __name__ == '__main__': 
     main() 
 
-"""from __future__ import division
-from random import SystemRandom
-
-cryptogen = SystemRandom()
-
-# find a prime
-p = 809
-
-# random g
-g = 256
-
-# private key d from {1..,p-2}
-d = 68
-
-# public key part
-k = (g ** d) % p
-
-
-## k_rpiv d
-## k_pub: p, g, k
-
-
-# message
-m = 100
-# r is any real number
-r = 89
-# encryption
-e = (g ** r) % p
-# cipher
-c = (k ** r * m) % p
-
-# decryption
-s = (e ** d) % p
-dec = (1/s * c) % p
-#dec = ((e ** d) ** (p - 2) * c) % p
-print(m, e, c, dec)"""
 
